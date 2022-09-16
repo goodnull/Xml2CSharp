6b405150e5a40b720ec3068c91275a2ae49f9b63
uhnname <>
2022-09-16 11:33:35 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+XmlToCSharp
+===========
+
+Site for Converting XML into XmlSerializer compatable C# Classes
+
+将xml转换成`XmlSerializer `序列化可用的c#实体类
---
+++ Xml2CSharp/ClassInfoWriter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Xml2CSharp
+{
+    public class ClassInfoWriter
+    {
+        private readonly IEnumerable<Class> _classInfo;
+
+        public ClassInfoWriter(IEnumerable<Class> classInfo)
+        {
+            _classInfo = classInfo;
+        }
+
+        public void Write(TextWriter textWriter)
+        {
+            using (textWriter)
+            {
+                textWriter.WriteLine("using System.Xml.Serialization;");
+                textWriter.WriteLine("");
+                textWriter.WriteLine("");
+                foreach (var @class in _classInfo.Reverse())
+                {
+                    textWriter.Write($"[XmlRoot(ElementName=\"{@class.XmlName}\"");
+                    textWriter.WriteLine(string.IsNullOrWhiteSpace(@class.Namespace) ? ")]" : $",Namespace=\"{@class.Namespace}\")]");
+                    textWriter.WriteLine("public class {0} {{", @class.Name);
+                    textWriter.WriteLine("");
+                    foreach (var field in @class.Fields)
+                    {
+                        textWriter.Write($"\t[Xml{field.XmlType}({field.XmlType}Name=\"{field.XmlName}\"");
+                        textWriter.WriteLine(string.IsNullOrWhiteSpace(field.Namespace) ? ")]" : $",Namespace=\"{field.Namespace}\")]");
+                        if (field.Type.ToLower() == "string")
+                            field.Type = field.Type.ToLower();
+                        textWriter.WriteLine($"\tpublic {field.Type} {field.Name}{{ get; set; }}");
+                        textWriter.WriteLine("");
+                    }
+                    textWriter.WriteLine("}");
+                    textWriter.WriteLine("");
+                }
+            }
+        }
+
+
+    }
+} 
